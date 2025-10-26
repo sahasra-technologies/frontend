e4b2cbc655941a0c6d3d4cbf3dae8529ab269776
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2025-10-26 21:37:21 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",   // allow external connections
-    port: 80,     // change to 3000 if you prefer
+    host: "::",
+    port: 3000,
   },
   plugins: [react()],
   resolve: {
