4d59fa48d87adea7e1df76793d2c404bb06ff1ee
VEMULA SHANATH KUMAR <65351126+shanathvemula@users.noreply.github.com>
2025-10-26 22:00:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   server: {
     host: "::",
-    port: 3000,
+    port: 80,      // run directly on port 80
+    strictPort: true, // fail if 80 is busy
   },
   plugins: [react()],
   resolve: {
